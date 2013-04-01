5209d972a4673a1d272543521ae5b3b543cd6155
numanilyas <numan.ilyas@gmail.com>
2013-04-01 17:00:27 -0400

    completing layout

--- spec/requests/static_pages_spec.rb
+++ spec/requests/static_pages_spec.rb
@@ -7,12 +7,12 @@ describe "Static Pages" do
   describe "Home page" do
     it "should have the content 'Sample App'" do
       # Run the generator again with the --webrat flag if you want to use webrat methods/matchers
-      visit '/static_pages/home'
+      visit root_path
       page.should have_content('Sample App')
     end
     
     it "should have the right title" do
-    visit '/static_pages/home'
+    visit root_path
     page.should have_selector('title',
                               :text => "#{base_title}Home")
     end
@@ -21,11 +21,11 @@ describe "Static Pages" do
   
   describe "Help Page" do
     it "should have content 'Help'" do
-      visit '/static_pages/help'
+      visit help_path
       page.should have_content('Help')
     end
     it "should have the title 'Help'" do
-      visit '/static_pages/help'
+      visit help_path
       page.should have_selector('title',
                         :text => "#{base_title}Help")
     end
@@ -33,12 +33,12 @@ describe "Static Pages" do
   
   describe "About page" do
     it "should have the content 'About Us'" do
-      visit '/static_pages/about'
+      visit about_path
       page.should have_content('About Us')
     end
     
     it "should have the title 'About Us'" do
-      visit '/static_pages/about'
+      visit about_path
       page.should have_selector('title',
                     :text => "#{base_title}About Us")
     end
@@ -47,12 +47,12 @@ describe "Static Pages" do
   
   describe "Contact page" do
     it "should have the content 'Contact'" do
-      visit '/static_pages/contact'
+      visit contact_path
       page.should have_content('Contact')
     end
     
     it "should have title 'Contact'" do
-      visit '/static_pages/contact'
+      visit contact_path
       page.should have_selector('title',
                     :text => "Ruby on Rails Tutorial Sample App | Contact")
     end
