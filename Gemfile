ada6c9354aef61746c5d23989083c59802df81f6
numanilyas <numan.ilyas@gmail.com>
2013-04-02 21:14:28 -0400

    basic user model

--- Gemfile
+++ Gemfile
@@ -3,6 +3,11 @@ source 'https://rubygems.org'
 gem 'rails', '3.2.13'
 gem 'bootstrap-sass', '2.1'
 gem 'pg', '0.12.2'
+gem 'bcrypt-ruby', '3.0.1'
+
+group :development do
+  gem 'annotate', '2.5.0'
+end
 
 group :development, :test do
   #gem 'sqlite3', '1.3.5'
--- db/schema.rb
+++ db/schema.rb
@@ -11,14 +11,16 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20130330153951) do
+ActiveRecord::Schema.define(:version => 20130402213223) do
 
   create_table "users", :force => true do |t|
     t.string   "name"
     t.string   "email"
-    t.string   "address"
-    t.datetime "created_at", :null => false
-    t.datetime "updated_at", :null => false
+    t.datetime "created_at",      :null => false
+    t.datetime "updated_at",      :null => false
+    t.string   "password_digest"
   end
+
+  add_index "users", ["email"], :name => "index_users_on_email", :unique => true
 
 end
