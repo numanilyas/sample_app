ac12a16bb796197031ace56549683a664da64b75
Numan Ilyas <numan.ilyas@gmail.com>
2013-05-01 16:08:40 -0400

    change

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -9,11 +9,11 @@ class UsersController < ApplicationController
   
   def show
     @user = User.find(params[:id])
-    @microposts = @user.microposts.paginate(page: params[:page])  
+    @microposts = @user.microposts.paginate(page: params[:page], :per_page => 10)  
   end
 
   def index
-    @users = User.paginate(page: params[:page])
+    @users = User.paginate(page: params[:page], :per_page => 10)
   end
 
   def destroy
